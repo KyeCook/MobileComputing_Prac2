df1cc27901cdf18dd83a86213a26ad0c1ef1fcbd
kyecook <acfanatics@hotmail.com>
2017-03-13 15:11:15 +1000

    Added in secondary 'settings' activity

--- app/src/main/java/com/example/kyecook/prac2/MainActivity.java
+++ app/src/main/java/com/example/kyecook/prac2/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.kyecook.prac2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -65,5 +67,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void settingsHandler (View view) {
+        Intent intent = new Intent(this, settingsActivity.class);
+        startActivity(intent);
     }
 }
